2dd5813a02e7fcdcc9ad1e111d0ee2b53524477e
CLP Development <115387383+ChriPi-Gamecraft@users.noreply.github.com>
2024-07-11 14:35:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+import discord
+from discord.ext import commands
+import yt_dlp
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+
+FFMPEG_OPTIONS = {
+    'options':'-vn',
+    'before_options':'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
+}
+YDL_OPTIONS = {
+'format': 'bestaudio', 'noplaylist': True
+}
+
+class MusicBot(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.queue = []
+        self.loop = False
+        self.current_song = None
+
+    @commands.hybrid_command(name="play", description="Spielt ein Lied ab")
+    async def play(self, ctx: commands.Context, * , search: str):
+        voice_channel = ctx.author.voice.channel if ctx.author.voice else None
+        if not voice_channel:
+          return await ctx.send("Du bist nicht in einem Sprachkanal")
+    
+        if not ctx.voice_client:
+         await voice_channel.connect()
+
+
+        async with ctx.typing():
+         with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+            try:
+                info = ydl.extract_info(f"ytsearch:{search}", download = False)
+            except Exception as e:
+                return await ctx.send(f"Fehler beim Abrufen von Informationen: {str(e)}")
+            
+            if 'entries' in info:
+                info = info['entries'][0]
+            url = info['url']
+            title  = info['title']
+            self.queue.append((url, title))
+            await ctx.send(f"Zur queue hinzugefügt: **{title}**")
+
+        if not ctx.voice_client.is_playing():
+         await self.play_next(ctx)
+
+    async def play_next(self, ctx:commands.Context):
+        if self.loop and self.current_song:
+          url, title = self.current_song
+        elif self.queue:
+          url, title = self.queue.pop(0)
+          self.current_song = (url, title)
+        else:
+          self.current_song = None
+          return await ctx.send("Queue ist leer!")
+       
+        source = await discord.FFmpegOpusAudio.from_probe(url, **FFMPEG_OPTIONS)
+        ctx.voice_client.play(source, after=lambda e: self.client.loop.create_task(self.play_next(ctx)))
+        await ctx.send(f"Gerade läuft: **{title}**")
+    @commands.hybrid_command(name="skip", description="Überspringt das aktuelle lied")
+    async def skip(self, ctx: commands.Context):
+       if ctx.voice_client and ctx.voice_client.is_playing():
+          ctx.voice_client.stop()
+          await ctx.send("Lied übersprungen!")
+
+
+client = commands.Bot(command_prefix="!", intents=intents)
+
+@client.event
+async def on_ready():
+   await client.tree.sync()
+   print("Bot ist Bereit")
+async def main():
+   await client.add_cog(MusicBot(client))
+   await client.start('DEIN TOKEN HIER')
+
+asyncio.run(main())
